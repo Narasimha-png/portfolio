69d7dbf84239df32c9b3fd2a78c97032b79e2e7c
Sreekanth Sree <121845000+sree9398@users.noreply.github.com>
2024-03-29 14:02:14 +0530

    files uploaded

---
+++ src/Details.js
@@ -0,0 +1,159 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpeg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+// import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import django from "./assets/techstack/django.png";
+import colab from "./assets/techstack/colab.jpeg";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import nodejs from "./assets/techstack/nodejs.png";
+import java from "./assets/techstack/java.jpeg";
+import python from "./assets/techstack/python.jpeg";
+import cpp from "./assets/techstack/cpp.webp";
+import sql from "./assets/techstack/sql.jpeg";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpeg";
+import projectImage3 from "./assets/projects/project3.jpg";
+//codingplatforms images
+import leetcode from "./assets/techstack/leetcode.jpeg";
+import codeforces from "./assets/techstack/codeforces.png";
+import gfg from "./assets/techstack/gfg.png";
+import codechef from "./assets/techstack/codechef.jpeg";
+import codingninjas from "./assets/techstack/codingninjas.jpeg";
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "SREEKANTH T",
+  tagline: "Competitive programming || Web Dev || MERN",
+  img: profile,
+  about: `I'm a 3rd-year B.Tech student at Sree Vidyanikethan Engineering College, Tirupati, studying Computer Science CSE-AIML. I'm passionate about competitive programming and skilled in solving problems. Additionally, I enjoy web development using the MERN stack, where I create dynamic and user-friendly applications.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/sree9398/",
+  github: "https://github.com/sree9398",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/sreekanth.sree__/",
+};
+
+
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Technology in Computer Science & Engineering with specialization AIML",
+    Company: "Sree Vidyanikethan Engineering College",
+    Location: "Tirupati , Andhra Pradesh",
+    Type: "Full Time",
+    Duration: "DEC 2021 - MAY 2025",
+  },
+  {
+    Position: "Intermediate",
+    Company: "Sri Chaitanya Junior College",
+    Location: "B.Thandrapadu , Kurnool , Andhra Pradesh",
+    Type: "Full Time",
+    Duration: "JUN 2019 - MAR 2021",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  django:django,
+  react: react,
+  bootstrap: bootstrap,
+  nodejs: nodejs,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  colab:colab,
+  github: github,
+  java:java,
+  python:python,
+  cpp:cpp,
+  sq:sql
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "SreeFoods - React-Based Food Ordering App ",
+    image: projectImage1,
+    description: `•	SreeFoods is a React-based application focused on making food ordering easy.`,
+    techstack: "HTML/CSS, JavaScript, React ",
+    previewLink: "/",
+    githubLink: "https://github.com/sree9398/SreeFoods",
+  },
+  {
+    title: "Real Estate Price Prediction Website ",
+    image: projectImage2,
+    description: `•	Developed a real estate price prediction website step-by-step. Utilized Bangalore home prices dataset from Kaggle.com.`,
+    techstack: "HTML/CSS, JavaScript, sklearn , linear regression",
+    previewLink: "/",
+    githubLink: "https://github.com/sree9398/Real-Estate-Price-Prediction-Website",
+  },
+  {
+    title: "Simple Mail Clone Web App",
+    image: projectImage3,
+    description: `	Developed a simple mail clone web app.`,
+    techstack: "Django (backend) and HTML, CSS, JavaScript, Bootstrap (frontend).",
+    previewLink: "/",
+    githubLink: "https://github.com/sree9398/Simple-Mail-Clone-Application",
+  },
+  
+];
+
+export const codingplatforms=[
+    {
+      title:'leetcode',
+      image: leetcode,
+      link:"https://leetcode.com/sree9398/"
+    },
+    {
+      title:'codeforces',
+      image: codeforces,
+      link:"https://codeforces.com/profile/sree9398"
+    },
+    {
+      title:'geeks for geeks',
+      image: gfg,
+      link:"https://auth.geeksforgeeks.org/user/sree9398"
+    },
+    {
+      title:'coding ninjas',
+      image: codingninjas,
+      link:"https://www.codingninjas.com/studio/profile/sree9398"
+    },
+    {
+      title:'codechef',
+      image: codechef,
+      link:"https://www.codechef.com/users/sree9398"
+    }
+];
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "telugusreekanth58@gmail.com",
+  phone: "+91 9398299515",
+};
